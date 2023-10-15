7c23e294848fb4b9771c28e0c8ca17ae1a96cd1f
pearsonjason <112111941+pearsonjason@users.noreply.github.com>
2023-10-15 15:39:56 -0400

    Update EmergencyRoomSim.java

--- datastructuresproblems/src/problem1/EmergencyRoomSim.java
+++ datastructuresproblems/src/problem1/EmergencyRoomSim.java
@@ -1,6 +1,17 @@
 package problem1;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class EmergencyRoomSim {
+	String name;
+	String color;
+
+	public Patient(String name, String color){
+		this.name = name;
+		this.color = color;
+	}
 	public static void main(String[] args) {
 		
 	}
