237cd0938d8be8b2adf337863d03e098f3c54e4b
pearsonjason <112111941+pearsonjason@users.noreply.github.com>
2023-10-17 11:38:53 -0400

    Update EmergencyRoomSim.java

--- datastructuresproblems/src/problem1/EmergencyRoomSim.java
+++ datastructuresproblems/src/problem1/EmergencyRoomSim.java
@@ -15,4 +15,22 @@ public class EmergencyRoomSim {
 	public static void main(String[] args) {
 		
 	}
+
+	public static void addPatient(Deque<Patient> queue, Scanner scanner) {
+        System.out.print("Enter patient's name: ");
+        String name = scanner.next();
+
+        System.out.print("Enter patient's color (red, blue, green): ");
+        String color = scanner.next();
+
+        Patient patient = new Patient(name, color);
+
+        if (color.equalsIgnoreCase("red")) {
+            queue.addFirst(patient); // Red patients are pushed to the front.
+        } else {
+            queue.addLast(patient); // Blue and green patients at the back.
+        }
+
+        System.out.println(name + " added to the queue.");
+    }
 }
